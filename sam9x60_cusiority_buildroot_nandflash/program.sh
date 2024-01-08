aae78ab3c80007a9344e426bec271038ac91e860
Patrick Lin <patrick.cclin@gmail.com>
2024-01-08 12:47:48 +0800

    Delete sam9x60_cusiority_buildroot_nandflash directory

--- sam9x60_cusiority_buildroot_nandflash/program.sh
+++
@@ -1,8 +0,0 @@
-sudo /opt/sam-ba_v3.7-linux_x86_64/sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c erase
-sudo /opt/sam-ba_v3.7-linux_x86_64/sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c writeboot:boot.bin
-sudo /opt/sam-ba_v3.7-linux_x86_64/sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c write:u-boot.bin:0x40000
-sudo /opt/sam-ba_v3.7-linux_x86_64/sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c write:uboot-env.bin:0x140000
-sudo /opt/sam-ba_v3.7-linux_x86_64/sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c write:sam9x60_curiosity.itb:0x200000
-sudo /opt/sam-ba_v3.7-linux_x86_64/sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c write:rootfs.ubi:0x800000
-
-sudo /opt/sam-ba_v3.7-linux_x86_64/sam-ba -p usb -d sam9x60:0:1 -a reset
