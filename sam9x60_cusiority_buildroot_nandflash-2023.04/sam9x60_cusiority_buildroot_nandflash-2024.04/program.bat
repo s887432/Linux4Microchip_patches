6c819aba1046872b6087ea96acf180f2b1512c84
Patrick Lin <patrick.cclin@gmail.com>
2024-09-27 23:34:58 +0800

    Delete sam9x60_cusiority_buildroot_nandflash-2023.04/sam9x60_cusiority_buildroot_nandflash-2024.04 directory

--- sam9x60_cusiority_buildroot_nandflash-2023.04/sam9x60_cusiority_buildroot_nandflash-2024.04/program.bat
+++
@@ -1,9 +0,0 @@
-sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c erase
-sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c writeboot:boot.bin
-sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c write:u-boot.bin:0x40000
-sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c write:uboot-env.bin:0x100000
-sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c write:uboot-env.bin:0x140000
-sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c write:sam9x60_curiosity.itb:0x180000
-sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c write:rootfs.ubi:0x800000
-
-sam-ba -p usb -d sam9x60:0:1 -a reset
--- sam9x60_cusiority_buildroot_nandflash-2023.04/sam9x60_cusiority_buildroot_nandflash-2024.04/program.sh
+++
@@ -1,9 +0,0 @@
-sudo /opt/sam-ba_v3.7-linux_x86_64/sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c erase
-sudo /opt/sam-ba_v3.7-linux_x86_64/sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c writeboot:boot.bin
-sudo /opt/sam-ba_v3.7-linux_x86_64/sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c write:u-boot.bin:0x40000
-sudo /opt/sam-ba_v3.7-linux_x86_64/sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c write:uboot-env.bin:0x100000
-sudo /opt/sam-ba_v3.7-linux_x86_64/sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c write:uboot-env.bin:0x140000
-sudo /opt/sam-ba_v3.7-linux_x86_64/sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c write:sam9x60_curiosity.itb:0x180000
-sudo /opt/sam-ba_v3.7-linux_x86_64/sam-ba -p usb -d sam9x60:0:1 -a nandflash:1:8:0xc2605007 -c write:rootfs.ubi:0x800000
-
-sudo /opt/sam-ba_v3.7-linux_x86_64/sam-ba -p usb -d sam9x60:0:1 -a reset
